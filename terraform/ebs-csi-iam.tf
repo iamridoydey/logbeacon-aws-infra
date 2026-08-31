e5c9b61e887e3f646dd7f42999c055290a96eabd
iamridoydey <iamridoydey@gmail.com>
2026-08-31 21:36:56 +0600

    Removed ebs csi role and policy resources and used attach_ebs_csi_policy which will automatically create by eks-pod-identity module

--- terraform/ebs-csi-iam.tf
+++ terraform/ebs-csi-iam.tf
@@ -1,47 +1,15 @@
-resource "aws_iam_role" "ebs_csi_role" {
-  name = "ebs-csi-role-${var.environment}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "pods.eks.amazonaws.com"
-        }
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-      }
-    ]
-  })
-
-  tags = {
-    Name        = "ebs-csi-role"
-    Environment = var.environment
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ebs_csi_attach" {
-  role       = aws_iam_role.ebs_csi_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEBSCSIDriverEKSClusterScopedPolicy"
-}
-
-
 module "ebs_csi_pod_identity" {
   source  = "terraform-aws-modules/eks-pod-identity/aws"
   version = "~> 1.0"
 
   name = "ebs-csi-pod-identity"
-
+  attach_aws_ebs_csi_policy = true
 
   associations = {
     ebs_csi = {
       cluster_name    = module.workload_eks.cluster_name
       namespace       = "kube-system"
       service_account = "ebs-csi-controller-sa"
-      role_arn = aws_iam_role.ebs_csi_role.arn
     }
   }
 
