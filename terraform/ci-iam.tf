09b48f4b3dda01fd87bae99b3cbf1e231c4c234a
iamridoydey <iamridoydey@gmail.com>
2026-08-31 15:34:46 +0600

    Added SsmAdminHostAccess policy to the logbeacon_infra_bootstrap_ci_role

--- terraform/ci-iam.tf
+++ terraform/ci-iam.tf
@@ -2,12 +2,12 @@
 # GITHUB OIDC PROVIDER
 # =============================================================
 module "iam_oidc_provider" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-oidc-provider"
+  source = "terraform-aws-modules/iam/aws//modules/iam-oidc-provider"
 
   url = "https://token.actions.githubusercontent.com"
 
   tags = {
-    Name = "github-oidc-provider"
+    Name        = "github-oidc-provider"
     Environment = var.environment
   }
 }
@@ -15,20 +15,20 @@ module "iam_oidc_provider" {
 # logbeacon-app repo ci role
 # =============================================================
 module "logbeacon_app_ci_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role"
-  name = "logbeacon-app-ci-role"
+  source = "terraform-aws-modules/iam/aws//modules/iam-role"
+  name   = "logbeacon-app-ci-role"
 
   enable_github_oidc = true
 
   oidc_wildcard_subjects = ["repo:iamridoydey/logbeacon-app:ref:refs/heads/main"]
 
   policies = {
-    EcrReadWrite = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser",
+    EcrReadWrite           = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser",
     SonarqubeCredReadWrite = aws_iam_policy.sonarqube_cred_read.arn
   }
 
   tags = {
-    Name = "logbeacon-app-ci-role"
+    Name        = "logbeacon-app-ci-role"
     Environment = var.environment
   }
 }
@@ -39,20 +39,47 @@ module "logbeacon_app_ci_role" {
 # logbeacon-infra repo ci role
 # =============================================================
 module "logbeacon_infra_bootstrap_ci_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role"
-  name = "logbeacon-infrabootstrap-ci-role"
+  source = "terraform-aws-modules/iam/aws//modules/iam-role"
+  name   = "logbeacon-infrabootstrap-ci-role"
 
   enable_github_oidc = true
 
   oidc_wildcard_subjects = ["repo:iamridoydey/logbeacon-aws-infra:ref:refs/heads/main"]
 
   policies = {
-    EksClusterPolicy = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-    WorkloadEksCredReadWritePolicy = aws_iam_policy.workload_cred_read_write.arn
+    EksClusterPolicy               = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    WorkloadEksCredReadWritePolicy = aws_iam_policy.workload_cred_read_write.arn,
+    SsmAdminHostAccess             = aws_iam_policy.infra_ci_ssm_access.arn
   }
 
   tags = {
-    Name = "logbeacon-infra-bootstrap-ci-role"
+    Name        = "logbeacon-infra-bootstrap-ci-role"
     Environment = var.environment
   }
+}
+
+# =============================================================
+# logbeacon-app repo ci role
+# =============================================================
+resource "aws_iam_policy" "infra_ci_ssm_access" {
+  name = "infra-ci-ssm-access-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ssm:StartSession",
+          "ssm:SendCommand",
+          "ssm:DescribeInstanceInformation",
+          "ssm:GetCommandInvocation"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
 }
