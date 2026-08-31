119f224d0fd95ee93b1bb4d6e303af16b6554fc3
iamridoydey <iamridoydey@gmail.com>
2026-08-31 21:39:52 +0600

    Added security_groups

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -21,6 +21,8 @@ resource "aws_instance" "logbeacon_admin" {
   subnet_id = module.vpc.private_subnets[0]
   associate_public_ip_address = false
 
+  security_groups = [aws_security_group.logbeacon_admin.id]
+
   iam_instance_profile = aws_iam_instance_profile.logbeacon_admin.name
 
 
