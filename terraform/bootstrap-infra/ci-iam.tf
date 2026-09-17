c5216a63f7a787155466a9fb3c80a71dd8fd86cf
iamridoydey <iamridoydey@gmail.com>
2026-09-17 17:19:34 +0600

    Added main-ci merge and destroy environment subject to iam role

--- terraform/bootstrap-infra/ci-iam.tf
+++ terraform/bootstrap-infra/ci-iam.tf
@@ -65,7 +65,9 @@ module "logbeacon_infra_bootstrap_ci_role" {
   enable_github_oidc = true
 
   oidc_wildcard_subjects = [
-    "repo:${var.github_username}@${var.github_account_id}/logbeacon-aws-infra@${var.github_repo_id}:ref:refs/heads/main"
+    "repo:${var.github_username}@${var.github_account_id}/logbeacon-aws-infra@${var.github_repo_id}:ref:refs/heads/main",
+    "repo:${var.github_username}@${var.github_account_id}/logbeacon-aws-infra@${var.github_repo_id}:environment:main-infra-production",
+    "repo:${var.github_username}@${var.github_account_id}/logbeacon-aws-infra@${var.github_repo_id}:environment:main-infra-destroy"
   ]
 
   policies = {
